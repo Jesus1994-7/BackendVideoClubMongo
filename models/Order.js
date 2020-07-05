41de2ff1b348e7f1cdc843faab837334887c3a0f
Jesus1994-7 <jesus1994_cg@hotmail.com>
2020-07-06 01:48:34 +0200

    Modificacion pedido

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,7 @@ const ObjectId = mongoose.Types.ObjectId;
 
 const OrderSchema = new mongoose.Schema({
     userId: ObjectId,
-    moviesId: [ObjectId],
+    movieId: ObjectId,
     deliveryDate: Date,
     returnDate: Date
 }, {timestamps : true})
